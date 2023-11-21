0e5befb55f2b5fd3533bfaa156bc9d2fee3c92b0
Christopher John <christopherlegacy01@gmail.com>
2023-11-21 09:39:24 -0800

    basic syntax

---
+++ first-program.py
@@ -0,0 +1,6 @@
+print("Hello world!")
+print('Hey, chris')
+print("""God is good
+love is awesome
+grace is sweet
+brrrrririririr""")
---
+++ user-input.py
@@ -0,0 +1,9 @@
+name = input("Type your name: ") #Demand user inputfor name 
+age = input ("Type your age: ") #Demand user inputfor name 
+message = f"Hey {name} and i guess your age is {age}" #output everything
+print(message) #print final message
+
+""""
+This is a type of comment use for multi line, it best usage is documentation or when you have
+a comprehensive expalnation to make.
+"""
